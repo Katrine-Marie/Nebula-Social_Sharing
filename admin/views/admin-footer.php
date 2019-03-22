9653ed62b8c5516ee158441d3e1be16d0b8601fe
Katrine-Marie <fjordscene@gmail.com>
2019-03-22 12:02:29 +0100

    Created common footer file

---
+++ admin/views/admin-footer.php
@@ -0,0 +1,3 @@
+<div class="nebula-footer">
+  Plugin: Nebula Social Sharing by <a href="https://fjordstudio.dk">Fjordstudio</a>  - <?php echo ' v'.NEBULA_SOCIAL_VERS.' - &copy;'.date("Y"); ?>
+</div>
