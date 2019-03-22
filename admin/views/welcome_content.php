068e456b3aaaae8c3da365920dfecf40ce3eb993
Katrine-Marie <fjordscene@gmail.com>
2019-03-22 12:01:18 +0100

    Added welcome content

---
+++ admin/views/welcome_content.php
@@ -0,0 +1,19 @@
+<div class="wrap admin-page">
+  <h1 class="title"><?php echo esc_html( get_admin_page_title() ); ?></h1>
+
+	<p>
+		Welcome to the Plugin Release Information Page for the Nebula Social Sharing plugin.
+	</p>
+
+	<h2>Social Signals</h2>
+
+	<p>
+		This plugin adds the social signals to individual posts and pages so that when they are shared, they include the post or page heading, an excerpt of the content, the featured image and links to your Twitter account. This makes the data effective for both Facebook and Twitter sharing.
+	</p>
+
+	<hr />
+
+	<p>A link to this page is at the bottom of the settings page if you want to view it again.</p>
+
+
+</div>
